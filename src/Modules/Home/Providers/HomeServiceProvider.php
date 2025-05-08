41cbf2f94cd252ee6346437e6004f3aec8f8074f
Raveesh Yadav <raveeshyadav8@outlook.com>
2025-05-09 00:12:16 +0530

    server fix

--- src/Modules/Home/Providers/HomeServiceProvider.php
+++ src/Modules/Home/Providers/HomeServiceProvider.php
@@ -27,6 +27,7 @@ class HomeServiceProvider extends ServiceProvider
         $this->registerConfig();
         $this->registerViews();
         $this->loadMigrationsFrom(module_path($this->name, 'database/migrations'));
+        $this->loadViewsFrom(module_path('Home', 'Resources/views'), 'home');
     }
 
     /**
